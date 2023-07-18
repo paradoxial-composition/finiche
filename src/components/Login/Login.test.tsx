f82fa7db5b464ebe1ff3e45ce259e8aa4ff905a0
paradoxial-composition <elbani.othman@gmail.com>
2023-07-18 15:50:52 +0200

    Add Login component logic and style.

---
+++ src/components/Login/Login.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, screen, fireEvent  } from '@testing-library/react';
+import Login from './Login';
+
+describe('Login component', () => {
+    render(<Login />);
+
+    const header = screen.getByRole('heading', { name: 'Finiche' });
+    const subHeader = screen.getByText('Un coin pour vos Film niche ;)');
+    const loginInput = screen.getByPlaceholderText('Identifiant Canal+');
+    const passwordInput = screen.getByPlaceholderText('Mot de passe');
+    const connectButton = screen.getByText('Se connecter');
+
+  test('renders the correct content', () => {
+
+    expect(header).toBeInTheDocument();
+    expect(subHeader).toBeInTheDocument();
+    expect(loginInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(connectButton).toBeInTheDocument();
+  });
+
+  test('has the correct class names and styles', () => {
+  
+    expect(header).toHaveClass('text-center');
+    expect(header).toHaveClass('text-4xl');
+    expect(header).toHaveClass('uppercase');
+    expect(subHeader).toHaveClass('text-center');
+    expect(loginInput).toHaveClass('bg-black');
+    expect(passwordInput).toHaveClass('bg-black');
+    expect(connectButton).toHaveClass('bg-pink');
+  });
+
+  test('handles user input correctly', () => {
+    render(<Login />);
+
+    fireEvent.change(loginInput, { target: { value: 'testuser' } });
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+
+    expect((loginInput as HTMLInputElement).value).toBe('testuser');
+    expect((passwordInput as HTMLInputElement).value).toBe('testpassword');
+  });
+
+  test('handles button click', () => {
+    render(<Login />);
+    console.log = jest.fn();
+    const connectButton = screen.getByText('Se connecter');
+
+    fireEvent.click(connectButton);
+    expect(console.log).toHaveBeenCalledWith('Logging in ..', { login: '', password: '' });
+  });
+});
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [userCredentials, setUserCredentials] = useState({login: '', password: ''});
+    
+    const handleAuthenticate = () => {
+        console.log('Logging in ..', userCredentials);
+        // TODO ADD Authentication async call and error handling
+        // Save Auth token in session storage, add the func in a helper.ts file
+    };
+
+    return (
+        <div className='w-full h-full flex flex-col justify-center items-center m-auto'>
+            <h1 className='w-fit p-1 text-center text-4xl uppercase'>Finiche</h1>
+            <span className='w-fit p-1 mb-2 text-center'>Un coin pour vos Film niche ;)</span>
+            <div className='w-4/5 max-w-lg flex flex-col items-center p-2 my-2 '>
+                <input 
+                    className='w-4/5 h-10 border border-slate-300 bg-black px-2 pl-3 m-auto my-2 rounded-lg'
+                    placeholder='Identifiant Canal+'
+                    onChange={(event) => setUserCredentials({...userCredentials, login: event.target.value})}
+                    />
+                <input 
+                    className='w-4/5 h-10 border border-slate-300 bg-black px-2 pl-3 m-auto my-2 rounded-lg' 
+                    placeholder='Mot de passe'
+                    type='password'
+                    onChange={(event) => setUserCredentials({...userCredentials, password: event.target.value})}
+                    />
+                <button className='w-32 h-10 bg-pink rounded-lg mt-4' onClick={handleAuthenticate}>Se connecter</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
