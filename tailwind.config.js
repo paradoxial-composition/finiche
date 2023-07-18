c5b7fc66ba2ce30dbd36311386cb1652237ff2d3
paradoxial-composition <elbani.othman@gmail.com>
2023-07-18 14:19:02 +0200

    Add tailwind dependencie and config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'black': '#0e0e0e',
+      'pink': '#ec3655',
+      'white': '#f7ffff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
